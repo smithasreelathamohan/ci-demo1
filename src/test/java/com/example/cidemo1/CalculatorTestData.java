44ff846d34f1d24056b1a84389cd1675cbcfb840
smithasreelathamohan <smitha.s11may@gmail.com>
2026-03-13 03:03:05 +0100

    corrected calc testdata

--- src/test/java/com/example/cidemo1/CalculatorTestData.java
+++ src/test/java/com/example/cidemo1/CalculatorTestData.java
@@ -12,14 +12,14 @@ public class CalculatorTestData {
                 Arguments.of(-7,-60,-67),
                 Arguments.of(-9,8,-1),
                 Arguments.of(80,-50,30),
-                Arguments.of(5,8.5,4),
+                Arguments.of(5,8.5,13.5),
                 Arguments.of(6,-50,-44),
                 Arguments.of(5.777,8.5,14.277000000000001));
     }
     public static Stream<Arguments> subtractionData() {
         return Stream.of(Arguments.of(17,5,12),
                 Arguments.of(-7,-7,0),
-                Arguments.of(-5,8,-3),
+                Arguments.of(-5,8,-13),
                 Arguments.of(-60,6,-66),
                 Arguments.of(5,17,-12),
                 Arguments.of(-78,-5,-73));
